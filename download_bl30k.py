3512fee481414d29ea69089315a2a96667bed019
haochenheheda <13021200713@163.com>
2022-04-17 17:51:42 +0200

    modify download scripts from "gdown" to "Drive API"

--- download_bl30k.py
+++ download_bl30k.py
@@ -1,8 +1,14 @@
 import os
 import gdown
 import tarfile
+import multiprocessing
 
+def Command(token, uid, output):
+    return 'curl -H "Authorization: Bearer {}" https://www.googleapis.com/drive/v3/files/{}?alt=media -o {}'.format(token,uid,output)
+def Script(token, uid, output):
+    os.system(Command(token, uid, output))
 
+token = 'ya29.A0ARrdaM9sod-Hdf6qv9wgbu_7b1F9sqrgKAo_N6_CsdSSVIJKnB2sKTdG9TYBvvxVnsdjmllJflaGU4qOlZoCFF_cEGHC2QPDxB2FaWpnrFCPu3DBsU7ZOeUehL-2JkuU3COoMAhio0S2xqpzACFaSYwHdXEP'
 LICENSE = """
 This dataset is a derivative of ShapeNet.
 Please read and respect their licenses and terms before use. 
@@ -20,31 +26,39 @@ if reply != 'y':
     exit()
 
 links = [
-    'https://drive.google.com/uc?id=1z9V5zxLOJLNt1Uj7RFqaP2FZWKzyXvVc',
-    'https://drive.google.com/uc?id=11-IzgNwEAPxgagb67FSrBdzZR7OKAEdJ',
-    'https://drive.google.com/uc?id=1ZfIv6GTo-OGpXpoKen1fUvDQ0A_WoQ-Q',
-    'https://drive.google.com/uc?id=1G4eXgYS2kL7_Cc0x3N1g1x7Zl8D_aU_-',
-    'https://drive.google.com/uc?id=1Y8q0V_oBwJIY27W_6-8CD1dRqV2gNTdE',
-    'https://drive.google.com/uc?id=1nawBAazf_unMv46qGBHhWcQ4JXZ5883r',
+    #'1z9V5zxLOJLNt1Uj7RFqaP2FZWKzyXvVc',
+    #'11-IzgNwEAPxgagb67FSrBdzZR7OKAEdJ',
+    #'1ZfIv6GTo-OGpXpoKen1fUvDQ0A_WoQ-Q',
+    #'1G4eXgYS2kL7_Cc0x3N1g1x7Zl8D_aU_-',
+    '1Y8q0V_oBwJIY27W_6-8CD1dRqV2gNTdE',
+    '1nawBAazf_unMv46qGBHhWcQ4JXZ5883r',
 ]
 
 names = [
-    'BL30K_a.tar',
-    'BL30K_b.tar',
-    'BL30K_c.tar',
-    'BL30K_d.tar',
+    #'BL30K_a.tar',
+    #'BL30K_b.tar',
+    #'BL30K_c.tar',
+    #'BL30K_d.tar',
     'BL30K_e.tar',
     'BL30K_f.tar',
 ]
 
 for i, link in enumerate(links):
     print('Downloading segment %d/%d ...' % (i, len(links)))
-    gdown.download(link, output='../%s' % names[i], quiet=False)
-    print('Extracting...')
-    with tarfile.open('../%s' % names[i], 'r') as tar_file:
-        tar_file.extractall('../%s' % names[i])
-    print('Cleaning up...')
-    os.remove('../%s' % names[i])
+    #gdown.download(link, output='../%s' % names[i], quiet=False)
+    os.system(Command(token, link, '../%s' % names[i]))
+#    print('Extracting...')
+#    with tarfile.open('../%s' % names[i], 'r') as tar_file:
+#        tar_file.extractall('../%s' % names[i])
+#    print('Cleaning up...')
+#    os.remove('../%s' % names[i])
 
+#pool = multiprocessing.Pool(processes=6)
+#for i, link in enumerate(links):
+#    print('Downloading segment %d/%d ...' % (i, len(links)))
+#    pool.map_async(Script,(token, link, '../%s' % names[i]))
+
+#pool.close()
+#pool.join()
 
 print('Done.')
--- download_model.py
+++ download_model.py
@@ -1,9 +1,12 @@
 import os
 import gdown
 
+def Command(token, uid, output):
+    return 'curl -H "Authorization: Bearer {}" https://www.googleapis.com/drive/v3/files/{}?alt=media -o {}'.format(token,uid,output)
+token = 'ya29.A0ARrdaM-yQnXaj8HFslo8WOafFie5QIezennC7erbCDoQqKcJQmHbBREv77FTg5dk7kiWv-yUh06mmqb96IfEOk6gY6ZHKsL3Ht7BXmkrVOrbWNAa2K0CTEAD2S5uHAfKvLiud-vb-rEvvdv6NDW6dRxwfYFO'
 
 os.makedirs('saves', exist_ok=True)
 print('Downloading stcn.pth...')
-gdown.download('https://drive.google.com/uc?id=1mRrE0uCI2ktdWlUgapJI_KmgeIiF2eOm', output='saves/stcn.pth', quiet=False)
-
+#gdown.download('https://drive.google.com/uc?id=1mRrE0uCI2ktdWlUgapJI_KmgeIiF2eOm', output='saves/stcn.pth', quiet=False)
+os.system(Command(token, '1mRrE0uCI2ktdWlUgapJI_KmgeIiF2eOm', 'saves/stcn.pth'))
 print('Done.')
