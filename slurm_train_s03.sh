053f47445b08997d75c4e4bdca68ba52a1a49971
Haochen Wang <hwang1@int3.local.snellius.surf.nl>
2022-04-22 11:45:20 +0200

    -

--- slurm_train_s03.sh
+++ slurm_train_s03.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 #SBATCH -N 1
 #SBATCH -p gpu
-#SBATCH --ntasks-per-node=4
-#SBATCH --gpus-per-node=2
+#SBATCH --ntasks-per-node=1
+#SBATCH --gpus-per-node=1
+#SBATCH --cpus-per-task=18
 #SBATCH --time=60:00:00
 ##SBATCH --exclusive
 
@@ -15,8 +16,8 @@ conda activate STCN
 
 #static images
 #CUDA_VISIBLE_DEVICES=0,1 OMP_NUM_THREADS=4 python -m torch.distributed.launch --master_port 9843 --nproc_per_node=2 train.py --id retrain_b16_g2_s0 --stage 0
-CUDA_VISIBLE_DEVICES=0 OMP_NUM_THREADS=4 python -m torch.distributed.launch --master_port 9842 --nproc_per_node=1 train.py --id retrain_b16_g1_s0 --stage 0 --batch_size 16 --num_workers 16
-#CUDA_VISIBLE_DEVICES=0 OMP_NUM_THREADS=4 python -m torch.distributed.launch --master_port 9844 --nproc_per_node=1 train.py --id retrain_b32_g1_s0 --stage 0 --batch_size 32 --num_workers 16 --lr 2e-5 --iterations 200000 --steps 100000
+#CUDA_VISIBLE_DEVICES=0 OMP_NUM_THREADS=4 python -m torch.distributed.launch --master_port 9842 --nproc_per_node=1 train.py --id retrain_b16_g1_s0 --stage 0 --batch_size 16 --num_workers 16
+CUDA_VISIBLE_DEVICES=0 OMP_NUM_THREADS=4 python -m torch.distributed.launch --master_port 9844 --nproc_per_node=1 train.py --id retrain_b32_g1_s0 --stage 0 --batch_size 32 --num_workers 16 --lr 2e-5 --iterations 200000 --steps 100000
 
 #main train on 150K
 #CUDA_VISIBLE_DEVICES=0,1 OMP_NUM_THREADS=4 python -m torch.distributed.launch --master_port 9842 --nproc_per_node=2 train.py --id retrain_s03 --load_network [path_to_trained_s0.pth] --stage 3
