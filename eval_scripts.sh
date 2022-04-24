8233aff6686afc4b8e17489d5d3d2f6168030cb7
haochenheheda <13021200713@163.com>
2022-04-24 03:00:40 +0200

    add scripts for multiple inference setting

---
+++ eval_scripts.sh
@@ -0,0 +1,10 @@
+#/bin/bash
+for i in 3 5 7 9 11 
+do
+python eval_youtube.py --output output_kmn${i}_res480 --memory_type kmn --sigma ${i}
+done
+
+for i in 3 5 7 9 11 
+do
+python eval_youtube.py --output output_kmn${i}_res600 --memory_type kmn --sigma ${i} --res 600
+done
